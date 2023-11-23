a96a52e5d2fc9fbdb69fa1290e669ed93a7e871d
darshitrabadiya <darshitrabadiya@darshits-mbp.bbrouter>
2023-11-23 07:57:00 +0530

    add new code

--- TestNgMavenExtentReport/src/main/java/TestNgDemo/TestNgMaven/App.java
+++ TestNgMavenExtentReport/src/main/java/TestNgDemo/TestNgMaven/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!123" );
+        System.out.println( "Hello World!123" );
+        System.out.println( "Hello World!123" );
+        System.out.println( "Hello World!123" );
+        
+        System.out.println("Login Page Test Added");
+        System.out.println("new code added by darshit for demo");
+        
     }
 }
